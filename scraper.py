af080792f28bfd69c766ba71684de7914f997ecd
Hache0099 <hache0099_github@protonmail.com>
2022-06-09 17:04:19 -0300

    Añadida la dataclass SearchResult

--- scraper.py
+++ scraper.py
@@ -1,5 +1,12 @@
-from bs4 import BeautifulSoup
+from dataclasses import dataclass, fields
 from SafeRequest.SafeRequest import safe_request
+
+@dataclass
+class MySearchResult:
+	videoId: str
+	title: str
+	lengthSeconds: int
+	author: str
 
 
 class YTScraper:
@@ -12,6 +19,7 @@ class YTScraper:
 #        print(new_req)
         params ={
                 "q": new_req,
+                "type": "video",
                 "pretty": 1,
                 "fields": "videoId,title,lengthSeconds,author",
                 }
@@ -22,14 +30,28 @@ class YTScraper:
 
     def __call__(self, request):
         results = self._get_result(request)
-#        parsed = BeautifulSoup(results, "html.parser")
         return results
 
+def _from_dict(cls, search_result):
+#	print(f"{search_result=}")
+	cls_vars = {f.name for f in fields(cls)}
+	filtered_vars = {k : v for k, v in search_result.items() if k in cls_vars}
+	print(f"{filtered_vars=}")
+	tmp = filtered_vars["lengthSeconds"]
+	filtered_vars["lengthSeconds"] = int(tmp)
+	
+	return cls(**filtered_vars)
+	
 
 # Esta sección es sólo para pruebas del módulo
 def main():
-    sc = YTScraper()
-    print(sc("doki doki waku waku"))
+	sc = YTScraper()
+#	print(sc("doki doki waku waku"))
+	result_list = sc("doki doki waku waku")
+	print(result_list)
+	result_cls_list = [_from_dict(MySearchResult, d) for d in result_list]
+	
+	print(result_cls_list)
 
 if __name__ == "__main__":
     main()
