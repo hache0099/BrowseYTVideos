efd06d4dba28923d7094f328778832e93ab6a8b9
Hache0099 <hache0099_github@protonmail.com>
2022-06-09 16:09:42 -0300

    Reemplazo de HTML por JSON

--- scraper.py
+++ scraper.py
@@ -3,14 +3,18 @@ from SafeRequest.SafeRequest import safe_request
 
 
 class YTScraper:
-    def __init__(self,yt_link = "https://yewtu.be/search"):
+    def __init__(self,yt_link = "https://vid.puffyan.us/api/v1/search"):
         self.yt_link = yt_link
     
 
     def _get_result(self, request):
         new_req = request.replace(" ", "+")
-        print(new_req)
-        params ={"q": new_req}
+#        print(new_req)
+        params ={
+                "q": new_req,
+                "pretty": 1,
+                "fields": "videoId,title,lengthSeconds,author",
+                }
         results = safe_request(self.yt_link, Data=params)
 
         return results
@@ -18,16 +22,14 @@ class YTScraper:
 
     def __call__(self, request):
         results = self._get_result(request)
-        parsed = BeautifulSoup(results, "html.parser")
-        return parsed.prettify()
+#        parsed = BeautifulSoup(results, "html.parser")
+        return results
 
 
 # Esta sección es sólo para pruebas del módulo
 def main():
     sc = YTScraper()
-    with open("doc.txt", mode="w") as f:
-        f.write(sc("doki doki waku waku"))
-
+    print(sc("doki doki waku waku"))
 
 if __name__ == "__main__":
     main()
