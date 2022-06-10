9746133e4b023ab314216d05fc1e26cb4533cb44
Hache0099 <hache0099_github@protonmail.com>
2022-06-10 00:56:39 -0300

    Eliminado el test del módulo

--- src/scraper.py
+++ src/scraper.py
@@ -44,6 +44,7 @@ def _from_dict(cls, search_result):
 	
 
 # Esta sección es sólo para pruebas del módulo
+"""
 def main():
 	sc = YTScraper()
 #	print(sc("doki doki waku waku"))
@@ -55,3 +56,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+"""
