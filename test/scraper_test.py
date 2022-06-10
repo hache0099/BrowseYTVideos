7261dcb3d97880666c80ecbab0a4dffa3f3ac0bb
Hache0099 <hache0099_github@protonmail.com>
2022-06-10 14:54:11 -0300

    Actualizado el test

--- test/scraper_test.py
+++ test/scraper_test.py
@@ -2,16 +2,22 @@ import sys
 
 sys.path.append("../src")
 
-from scraper import YTScraper, _from_dict
+from scraper import YTScraper, MySearchResult
 
 def main():
 	sc = YTScraper()
 #	print(sc("doki doki waku waku"))
 	result_list = sc("doki doki waku waku")
-	print(result_list)
-	result_cls_list = [_from_dict(d) for d in result_list]
+#	print(result_list)
 	
-	print(result_cls_list)
+	for result in result_list:
+		assert isinstance(result, MySearchResult)
+		assert isinstance(result.lenVid, int)
+		
+		print(result)
+	# result_cls_list = [_from_dict(d) for d in result_list]
+	
+	# print(result_cls_list)
 
 
 if __name__ == "__main__":
