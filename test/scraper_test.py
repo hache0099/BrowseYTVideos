7a81a9dafabcb632323d57b4ed27bdce2f7e1649
Hache0099 <hache0099_github@protonmail.com>
2022-06-10 00:55:06 -0300

    Creado test para scraper.py

---
+++ test/scraper_test.py
@@ -0,0 +1,14 @@
+from scraper import YTScraper, MySearchResult
+
+def main():
+    sc = YTScraper()
+#	print(sc("doki doki waku waku"))
+	result_list = sc("doki doki waku waku")
+	print(result_list)
+	result_cls_list = [_from_dict(MySearchResult, d) for d in result_list]
+	
+	print(result_cls_list)
+
+
+if __name__ == "__main__":
+    main()
