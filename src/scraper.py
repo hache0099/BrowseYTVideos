2e484d3e0f0ba6e7a760749292335b5fe1142de8
Hache0099 <hache0099_github@protonmail.com>
2022-06-10 15:01:19 -0300

    Borradas líneas comentadas

--- src/scraper.py
+++ src/scraper.py
@@ -52,18 +52,3 @@ def _from_dict(search_result):
 	
 	return MySearchResult(**filtered_vars)
 	
-
-# Esta sección es sólo para pruebas del módulo
-"""
-def main():
-	sc = YTScraper()
-#	print(sc("doki doki waku waku"))
-	result_list = sc("doki doki waku waku")
-	print(result_list)
-	result_cls_list = [_from_dict(MySearchResult, d) for d in result_list]
-	
-	print(result_cls_list)
-
-if __name__ == "__main__":
-    main()
-"""
