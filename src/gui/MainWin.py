75bc15dca8493efe40b9487e77c0b43c43f25274
Hache0099 <hache0099_github@protonmail.com>
2022-06-17 13:05:53 -0300

    Añadido los parámetros ancho y alto a la ventana

--- src/gui/MainWin.py
+++ src/gui/MainWin.py
@@ -1,12 +1,12 @@
 import gi
-gi.require_version("Gtk","3.0")
+
+gi.require_version("Gtk", "3.0")
 from gi.repository import Gtk, Gio, GLib
 
-class YTWin(Gtk.Window):
-	def __init__(self, win_title: str):
-		super().__init__(title=win_title)
-		
-		
-		self.show_all()
-		self.connect("destroy", Gtk.main_quit)
 
+class YTWin(Gtk.Window):
+    def __init__(self, win_title: str, w, h):
+        super().__init__(title=win_title, default_width=w, default_height=h)
+
+        self.show_all()
+        self.connect("destroy", Gtk.main_quit)
