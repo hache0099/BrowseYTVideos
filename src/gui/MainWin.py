6ca8954478e1b89fa1c2c8611d7a33465ca98b32
Hache0099 <hache0099_github@protonmail.com>
2022-06-17 15:27:51 -0300

    Añadiendo los demás elementos de la GUI

--- src/gui/MainWin.py
+++ src/gui/MainWin.py
@@ -4,9 +4,22 @@ gi.require_version("Gtk", "3.0")
 from gi.repository import Gtk, Gio, GLib
 
 
-class YTWin(Gtk.Window):
-    def __init__(self, win_title: str, w, h):
-        super().__init__(title=win_title, default_width=w, default_height=h)
 
-        self.show_all()
-        self.connect("destroy", Gtk.main_quit)
+class YTWin(Gtk.Window):
+	def __init__(self, win_title: str, w, h):
+		super().__init__(title=win_title, default_width=w, default_height=h)
+		
+		self.cancellable = Gio.Cancellable()
+		
+		self.search = Gtk.SearchEntry()
+		self.results = Gtk.CellView()
+		
+		box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=6,
+			      border_width=12)
+		
+		box.pack_start(self.search, False, True, 0)
+		box.pack_start(self.results, False, True, 0)
+			
+		self.add(box)
+		self.show_all()
+		self.connect("destroy", Gtk.main_quit)
