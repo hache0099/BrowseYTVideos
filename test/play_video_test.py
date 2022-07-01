97fd26f6c4bf1ee041e327d773c391e6769c7c40
Hache0099 <hache0099_github@protonmail.com>
2022-07-01 01:47:25 -0300

    Actualizado el test

--- test/play_video_test.py
+++ test/play_video_test.py
@@ -2,10 +2,19 @@ import sys
 
 sys.path.append("../src")
 
+import pytest
 from play_video import YTPlayer
+from custom_exceptions import ProcessVideoError
 
 player = YTPlayer()
 
 
 def test_video():
-    player.set_video("-1YmyJW0O6o")
+    player.set_video("-1YmyJW0O6o") # Adivina la canción
+    player.play_video()
+
+
+def test_invalid_id():
+    player.set_video("no")
+    with pytest.raises(ProcessVideoError):
+        player.play_video()
