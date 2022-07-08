b6c962846c0ee12132fd9ac372f37b9306e80f5c
Hache0099 <hache0099_github@protonmail.com>
2022-07-08 10:55:34 -0300

    Añadida la ventana principal al módulo

--- src/main.py
+++ src/main.py
@@ -1,8 +1,16 @@
-import youtube_dl
+# ~ import youtube_dl
+import gi
+
+gi.require_version("Gtk", "3.0")
+from gi.repository import Gtk
+from MainWin import YTWin
 
 def main():
-    with youtube_dl.YoutubeDL({}) as ydl:
-        pass
+    # ~ with youtube_dl.YoutubeDL({}) as ydl:
+        # ~ pass
+    win = YTWin("YTSearch", 500, 500)
+    
+    Gtk.main()
 
 
 if __name__ == "__main__":
